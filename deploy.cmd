b2ce2578ca9472acbae5dacbe0e1a6264ddcf1d9
Ronaldo Canesqui <ronaldo.canesqui@eldorado.org.br>
2018-04-17 10:19:52 -0300

    Installing dependencies on wwwroot

--- deploy.cmd
+++ deploy.cmd
@@ -120,7 +120,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 ::
 :: 5. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;node_modules;\src"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;node_modules;src"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
