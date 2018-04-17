3bea83973854186626f2cdf5da8b230b3188cb71
Ronaldo Canesqui <ronaldo.canesqui@eldorado.org.br>
2018-04-17 09:52:56 -0300

    Adding directory to ignore list

--- deploy.cmd
+++ deploy.cmd
@@ -120,7 +120,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 ::
 :: 5. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;node_modules"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
