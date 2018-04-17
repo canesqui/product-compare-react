b9da73135a6114a811447c45885d968911b9e155
Ronaldo Canesqui <ronaldo.canesqui@eldorado.org.br>
2018-04-17 08:05:51 -0300

    Installing dependencies on DEPLOYMENT_SOURCE and adjusting KUDU_SYNC_CMD order

--- deploy.cmd
+++ deploy.cmd
@@ -93,20 +93,21 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-::
-:: 1. KuduSync
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-
-:: 2. Select node version
+:: 1. Select node version
 call :SelectNodeVersion
 
-:: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
+:: 2. Install npm packages - production dependencies --ignoring NODE_ENV = production set by KUDU by default
+IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+  pushd "%DEPLOYMENT_SOURCE%"
+  call :ExecuteCmd !NPM_CMD! install --production
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+:: 3. Install npm packages - dev dependencies --ignoring NODE_ENV = production set by KUDU by default
+IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+  pushd "%DEPLOYMENT_SOURCE%"
+  call :ExecuteCmd !NPM_CMD! install --ONLY=dev
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
@@ -115,10 +116,11 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   call :ExecuteCmd !NPM_CMD! run build
   IF !ERRORLEVEL! NEQ 0 goto error
 
-:: 5. Copy Build to wwwroot
-::  call :ExecuteCmd xcopy /s "%DEPLOYMENT_SOURCE%\Build" "%DEPLOYMENT_TARGET%"
-::  IF !ERRORLEVEL! NEQ 0 goto error
-  
+::
+:: 5. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 
