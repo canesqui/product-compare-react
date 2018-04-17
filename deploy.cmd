6e58617fb50e93bcd182b3f970354525f054f753
Ronaldo Canesqui <ronaldo.canesqui@eldorado.org.br>
2018-04-17 09:11:02 -0300

    Adding files to ignore list

--- deploy.cmd
+++ deploy.cmd
@@ -55,14 +55,14 @@ goto Deployment
 :SelectNodeVersion
 
 IF NOT DEFINED WEBSITE_NODE_DEFAULT_VERSION (
-	SET WEBSITE_NODE_DEFAULT_VERSION = "8.11"
+	SET WEBSITE_NODE_DEFAULT_VERSION = "8.11.1"
 )
 
 
 IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
   :: The following are done only on Windows Azure Websites environment
-  call %KUDU_SELECT_NODE_VERSION_CMD% "%DEPLOYMENT_SOURCE%" "%DEPLOYMENT_TARGET%" "%DEPLOYMENT_TEMP%"
-  IF !ERRORLEVEL! NEQ 0 goto error
+  ::call %KUDU_SELECT_NODE_VERSION_CMD% "%DEPLOYMENT_SOURCE%" "%DEPLOYMENT_TARGET%" "%DEPLOYMENT_TEMP%"
+  ::IF !ERRORLEVEL! NEQ 0 goto error
 
   IF EXIST "%DEPLOYMENT_TEMP%\__nodeVersion.tmp" (
     SET /p NODE_EXE=<"%DEPLOYMENT_TEMP%\__nodeVersion.tmp"
@@ -119,7 +119,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 ::
 :: 5. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;src;scripts;public;config;package.json;LICENSE;README.md;.gitignore;.eslintrc"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
